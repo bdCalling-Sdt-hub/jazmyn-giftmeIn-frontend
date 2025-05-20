7d5b5322e9e39c7e273d355753ca9f242722633d
Mahmud Hasan Sabbir <mahmoodsabbir3087@gmail.com>
2025-05-20 18:43:55 +0600

    chore: update base URL to new server IP address
    The base URL has been updated from `167.71.173.195` to `64.23.193.89` in both `.env` and `baseApi.ts` files to reflect the new server IP address. This change ensures the application points to the correct backend server.

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -4,7 +4,8 @@ const api = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
     // baseUrl: 'http://10.0.70.188:5004/api/v1/',
-    baseUrl: 'http://167.71.173.195:5004/api/v1/',
+    // baseUrl: 'http://167.71.173.195:5004/api/v1/',
+    baseUrl: 'http://64.23.193.89:5004/api/v1/',
 
     prepareHeaders: (headers) => {
       const token = localStorage.getItem('authToken') || sessionStorage.getItem('authToken');
